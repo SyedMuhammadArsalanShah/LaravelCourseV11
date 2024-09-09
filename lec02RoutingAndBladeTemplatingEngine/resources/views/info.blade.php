88f4275ac8283ed358610db8cb528dfb0e247970
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2024-09-09 21:00:50 +0500

    code updated

--- lec02RoutingAndBladeTemplatingEngine/resources/views/info.blade.php
+++ lec02RoutingAndBladeTemplatingEngine/resources/views/info.blade.php
@@ -14,6 +14,85 @@
     <h1>{{ $id }}</h1>
     <h1> {!! $student !!}</h1>
 
+    <h1>{{ 'this is my web' }}</h1>
+
+    <h1> {{ time() }}</h1>
+    <h1> {{ date('m-d-y') }}</h1>
+
+
+
+
+
+
+    @if ($name == 'ali')
+        {{ 'this is an admin account' }}
+    @elseif ($name == 'fatimah')
+        {{ 'this is user account' }}
+    @else
+        {{ ' create an account ' }}
+    @endif
+
+
+
+    @unless ($name == 'sajid')
+        {{ 'this verified account ' }}
+    @endunless
+
+
+
+    @isset($id)
+        {{ 'user is from view ' }}
+    @endisset
+
+
+
+    <ul>
+
+        @for ($i = 0; $i <= 10; $i++)
+            <li> {{ $i }} </li>
+        @endfor
+
+
+    </ul>
+
+
+    @php
+
+        $j = 1;
+
+    @endphp
+
+    @while ($j <= 10)
+        {{ 'index' . $j }}
+
+        @php
+            $j++;
+        @endphp
+    @endwhile
+
+
+
+
+
+
+    @php
+
+        $students = ['ali', 'fatimah', 'hasan', 'hussain'];
+    @endphp
+
+
+
+    @foreach ($students as $item)
+        {{ $item }}
+    @endforeach
+
+
+
+
+
+
+
+
 </body>
 
 </html>
